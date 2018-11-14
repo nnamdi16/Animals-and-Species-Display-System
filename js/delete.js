3dc697468355501c95366840a69d8bc25e9c5ce9
nnamdi16 <nwabuokeinnamdi@gmail.com>
2018-11-14 11:42:40 +0100

    Updates from the work

--- js/delete.js
+++ js/delete.js
@@ -1,6 +1,6 @@
 
 let url = new URL(document.location.href);
-let counter  = 5;
+let counter  = 6;
 let i = url.searchParams.get("id");
 let path = `http://localhost:3000/adminPost/${counter}`;
 function Delete(){
@@ -19,11 +19,33 @@ function Delete(){
   
 }
 
+  $.get(
+    `http://localhost:/adminPost`,
+    function(data){
+      let 
+    }
+    
+  )
+
 function Update(){
+  ++counter;
+  console.log(counter);
+  const animalName = $("#animal-name").val();
+	const animalSpecies =$("#animal-species").val();
+	const description = $("#animal-description").val();
+	const pictureUrl = $("#animal-pictureUrl").val();
+  const data = {
+    animalName:animalName,
+    animalSpecies:animalSpecies,
+    pictureUrl: description,
+    description: pictureUrl,
+    
+  }
   $.ajax(
     {
       url:path,
       method:'PUT',
+      data:data,
       success:function(response){
         alert("Update completed");
         document.location.href = "gallery.html";
@@ -34,3 +56,7 @@ function Update(){
     }
   )
 }
+
+// $("#update-submit").click(function(){
+//   Update();
+// })
---
+++ js/user.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+	let i = Math.random()
+	let url = new URL(document.location.href);
+	 i = url.searchParams.get("i");
+	let path = `http://localhost:3000/adminPost/${i}`;
+
+
+	$.get(
+		`http://localhost:3000/adminPost`,
+
+	
+		function(data) {
+		  for (counter in data) {
+				console.log(counter);
+			$("#table").append(
+			  `<tr><td>
+				${data[counter].animalName}</td><td>${data[counter].animalSpecies}</td><td>${data[counter].pictureUrl}</td><td>${data[counter].description}</td><td>
+				<a href='./delete.html?id=${data[counter].id}'></a>
+				<button type='button'class="btn btn-primary"  onClick = "Delete(counter)">DELETE</button><button type='button' class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">UPDATE</button></td><tr>`
+			);
+		  }
+		},
+		"json"
+		);
+
+
+		
+		
+
+		
+
+	 
+})
